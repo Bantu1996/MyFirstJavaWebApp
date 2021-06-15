54a8ec39943163b625c883c13f2fee83154059ff
Bantu Sotyato <sotyatobantu@gmail.com>
2021-06-15 16:09:19 +0200

    added radio buttons

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,9 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 public class App {
@@ -7,16 +13,46 @@ public class App {
             staticFiles.location("/public");
             port(2020);
             get("/greet", (req, res) -> "Hello");
-            post("/greet/:username", (req, res) -> {
 
+            post("/greet/:username", (req, res) -> {
                 return "Hello " + req.queryParams("username");
             });
 
-
-
             get("/greet/:username", (request, response) -> {
-                return "Hello: " + request.params(":username");
+                return "Hello " + request.params("username");
             });
+
+
+
+            get("/hello", (req, res) -> {
+                Map<String, Object> map = new HashMap<>();
+                return new ModelAndView(map, "hello.handlebars");
+            }, new HandlebarsTemplateEngine());
+
+
+            post("/hello", (req, res) -> {
+                String message = new String();
+                Map<String, Object> map = new HashMap<>();
+                // create the greeting message
+                String greeting = req.queryParams("username");
+                String language =  req.queryParams("radioBtn");
+                if (language.equals("English")){
+                    message = "Hello, " + greeting;
+                }
+                else if (language.equals("IsiXhosa")){
+                    message = "Molo, " + greeting;
+                }
+                else {
+                    message = "Goeie dae, " + greeting;
+                }
+
+                // put it in the map which is passed to the template - the value will be merged into the template
+                map.put("greeting", message);
+                return new ModelAndView(map, "hello.handlebars");
+            }, new HandlebarsTemplateEngine());
+
+
+
             get("/greet/:username/language/:English", (request, response) -> {
                 return "Hello " + request.params("username") + " you are greeted in " + request.params("English");
             });
@@ -24,7 +60,7 @@ public class App {
                 return "Molo " + request.params("username") + " ubulisiwe ngesi " + request.params("Xhosa");
             });
             get("/greet/:username/language/:Afrikaans", (request, response) -> {
-                return "Halo " + request.params("username") + " jy is groet van " + request.params("Afrikaans");
+                return "Goeie Dae " + request.params("username") + " jy is groet van " + request.params("Afrikaans");
             });
         }
     }
